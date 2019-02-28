2df321f8295703121a1f5a7562f7adc23ce045dc
rfranci52 <46037634+rfranci52@users.noreply.github.com>
2019-02-28 16:41:41 -0500

    Add files via upload
    added initial nasa api

---
+++ project8_nasa.js
@@ -0,0 +1,26 @@
+var url = "https://api.nasa.gov/planetary/earth/imagery/?lon=-73.926421&lat=40.826562&date=2017-2-02&cloud_score=True&api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo";
+// var url = "https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo";
+
+
+$.ajax({
+    url: url,
+    success: function (result) {
+        if ("copyright" in result) {
+            $("#copyright").text("Image Credits: " + result.copyright);
+        } else {
+            $("#copyright").text("Image Credits: " + "Public Domain");
+        }
+
+        if (result.media_type == "video") {
+            $("#apod_img_id").css("display", "none");
+            $("#apod_vid_id").attr("src", result.url);
+        } else {
+            $("#apod_vid_id").css("display", "none");
+            $("#apod_img_id").attr("src", result.url);
+        }
+        $("#reqObject").text(url);
+        $("#returnObject").text(JSON.stringify(result, null, 4));
+        $("#apod_explaination").text(result.explanation);
+        $("#apod_title").text(result.title);
+    }
+});
