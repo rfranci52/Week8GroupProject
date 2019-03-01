2fd134b00009318560931815ea4b2989e824cad8
drywitte <Dan@Bens-iMac.local>
2019-02-28 20:32:18 -0500

    retrieving lat & long

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,18 @@
 var map;
 var lat;
-var long;
+var lng;
 
 function initMap() {
+    // create map
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 8
+    });
+
+    // detect map movement to get new coords
+    map.addListener('dragend', function() {
+        lat = map.getCenter().lat();
+        lng = map.getCenter().lng();
+        console.log(`${lat} and ${lng}`)
     });
 }
