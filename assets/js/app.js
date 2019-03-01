83edfd74c30d4fba00d7c2d8bfce01d7c5a9da4d
drywitte <Dan@Bens-iMac.local>
2019-02-28 20:17:47 -0500

    cleanup a comment

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
-// $(document).ready( function() {
-    var map;
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
-        });
-    }
-// });
+var map;
+var lat;
+var long;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
