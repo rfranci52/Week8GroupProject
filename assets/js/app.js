df93893a717b374a710dcfbee0d8a2d1501427a4
drywitte <Dan@Bens-iMac.local>
2019-02-28 22:25:02 -0500

    working autocomplete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 var map;
 var lat;
 var lng;
+var place;
+var autocomplete;
 
 function initMap() {
     // create map
@@ -16,5 +18,15 @@ function initMap() {
         console.log(`${lat} and ${lng}`)
     });
 
-    service = new google.maps.places.Autocomplete(document.getElementById('input'));
+    autocomplete = new google.maps.places.Autocomplete(document.getElementById('input'));
+    autocomplete.bindTo('bounds', map);
+    autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
+
+    autocomplete.addListener('place_changed', function() {
+        place = autocomplete.getPlace();
+        map.setCenter(place.geometry.location);
+        lat = map.getCenter().lat();
+        lng = map.getCenter().lng();
+        console.log(`${lat} and ${lng}`)
+    });
 }
