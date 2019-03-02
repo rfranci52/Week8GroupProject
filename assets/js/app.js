b2690fd92ed0e7a7237cb65709bf2187585c05c6
hubenschmidt <whubenschmidt@gmail.com>
2019-03-02 12:30:32 -0500

    working JSON overlay example

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,14 +7,14 @@ var autocomplete;
 function initMap() {
     // create map
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: { lat: 2.8, lng: -187.3 },
+        zoom: 3
     });
 
-    placeMarker({lat: -34.397, lng: 150.644}, map);
+    placeMarker({ lat: -34.397, lng: 150.644 }, map);
 
     // detect map movement to get new coords
-    map.addListener('dragend', function() {
+    map.addListener('dragend', function () {
         lat = map.getCenter().lat();
         lng = map.getCenter().lng();
         console.log(`${lat} and ${lng}`)
@@ -26,7 +26,7 @@ function initMap() {
     autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
 
     // handle autcomplete to get coords
-    autocomplete.addListener('place_changed', function() {
+    autocomplete.addListener('place_changed', function () {
         place = autocomplete.getPlace();
         map.setCenter(place.geometry.location);
         lat = map.getCenter().lat();
@@ -34,9 +34,9 @@ function initMap() {
         console.log(`${lat} and ${lng}`)
     });
 
-     //add marker to map 
-     map.addListener('dblclick', function (e) {
-        placeMarker(e.latLng);        
+    //add marker to map 
+    map.addListener('dblclick', function (e) {
+        placeMarker(e.latLng);
     });
 
     //place marker on map
@@ -46,8 +46,32 @@ function initMap() {
             map: map,
             draggable: false
         });
-    
+
+
+        var script = document.createElement('script');
+        // This example uses a local copy of the GeoJSON stored at
+        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+        script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+        document.getElementsByTagName('head')[0].appendChild(script);
     }
 
-   
+    // Loop through the results array and place a marker for each
+    // set of coordinates.
+    window.eqfeed_callback = function (results) {
+        for (var i = 0; i < results.features.length; i++) {
+            var coords = results.features[i].geometry.coordinates;
+            var latLng = new google.maps.LatLng(coords[1], coords[0]);
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: map
+            });
+        }
+    }
+
 }
+
+
+
+
+
+
