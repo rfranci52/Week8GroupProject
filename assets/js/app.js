fc985cbed689002e0c42c5f54c06ca5a9d222a3f
hubenschmidt <45466192+hubenschmidt@users.noreply.github.com>
2019-03-03 19:05:07 -0500

    Add files via upload
    make JSON string from getOpenData(), parse elements from getOpenData()

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,64 @@ var lat;
 var lng;
 var place;
 var autocomplete;
+// var healthTheme = [];
+
+
+var items = [];
 var healthTheme = [];
+var healthDescription = [];
+var distributionArr = [];
+var healthIdentifier = [];
+var healthLandingPage = [];
+var healthDateModified = [];
+
+
+getOpenData();
+
+function getOpenData() {
+    //JSON response for all APIs with "Health" theme
+    var url = "https://data.cityofnewyork.us/data.json";
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).then(function (response) {
+        var dataArr = response.dataset;
+        dataArr.forEach(function (element) {
+            if (element.theme == 'Health') {
+                items.push(element)
+                healthTheme.push(element.theme);
+                healthDescription.push(element.description);
+                distributionArr.push(element.distribution[2]);
+                healthIdentifier.push(element.identifier);
+                healthLandingPage.push(element.landingPage)
+                healthDateModified.push(element.modified)
+            }
+        })
+        console.log(items);
+        console.log(healthTheme);
+        console.log(healthDescription);
+        console.log(distributionArr);
+        console.log(healthIdentifier);
+        console.log(healthLandingPage);
+        console.log(healthDateModified);
+    })
+    makeJSON();
+}
+
+//make usable JSON string from OpenData
+function makeJSON(){
+    var str1 = "https://data.cityofnewyork.us/api/views/rsgh-akpg";
+    var res = str1.slice(40) + ".json";
+    console.log(res);
+    var str2 = "https://data.cityofnewyork.us/resource/"
+    var concatStr = str2.concat(res);
+    console.log(concatStr);
+}
 
 function initMap() {
     // create map
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.730610, lng: -73.935242},
+        center: { lat: 40.730610, lng: -73.935242 },
         zoom: 13
     });
 
@@ -39,34 +91,20 @@ function initMap() {
         url: url,
         method: "GET"
     }).then(function (results) {
-            for (var i = 0; i < results.length; i++) {
-                    if (results[i].city === "Brooklyn"){
-                        console.log(results[i]);
-                        console.log(results[i].name_1, results[i].city)
-                        var lat = results[i].latitude;
-                        var lng = results[i].longitude;
-                        var latLng = new google.maps.LatLng(lat, lng);
-                        placeMarker(latLng);
-                    }
-            }
-    })
-
-    //JSON response for all APIs with "Health" theme
-    var url = "https://nycopendata.socrata.com/data.json";
-    $.ajax({
-        url: url,
-        method: "GET"
-    }).then(function (response) {
-        var dataArr = response.dataset;
-        // var healthTheme = [];
-        dataArr.forEach(function(element){
-            if(element.theme == 'Health'){
-                healthTheme.push(element)
-            }   
-        })
-        console.log(healthTheme)
+        for (var i = 0; i < results.length; i++) {
+            // if (results[i].city === "Brooklyn") {
+                // console.log(results[i]);
+                // console.log(results[i].name_1, results[i].city)
+                var lat = results[i].latitude;
+                var lng = results[i].longitude;
+                var latLng = new google.maps.LatLng(lat, lng);
+                placeMarker(latLng);
+            // }
+        }
     })
 }
+
+
 
 
 //place marker on map
@@ -79,12 +117,12 @@ function placeMarker(position) {
 
     var infowindow = new google.maps.InfoWindow({
         content: "A place!"
-      });
+    });
 
-    marker.addListener('click', function() {
+    marker.addListener('click', function () {
         infowindow.open(map, marker);
     });
 
     return marker;
 }
-        
+
